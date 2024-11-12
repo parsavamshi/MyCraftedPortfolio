aae2e046798c2be54169c038ea4bd71f507827a8
parsavamshi <84668148+parsavamshi@users.noreply.github.com>
2024-11-12 17:20:02 +0530

    design completed

--- js/script.js
+++ js/script.js
@@ -7,25 +7,79 @@ var typed = new Typed(".typing", {
 })
 /* ========================   Aside  ========================================*/
 const nav = document.querySelector(".nav"),
-  navList = nav.querySelectorAll("li"),
-  totalNavList = navList.length;
-  for(let i=0; i<totalNavList; i++){
-    // console.log(navList[i])
-    const  a= navList[i].querySelector("a")
-    console.log(a)
-    a.addEventListener('click', function(){
-      // console.log(this)
-      for(let j=0; j<totalNavList; j++){
-        navList[j].querySelector("a").classList.remove("active");
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length,
+      allSection = document.querySelectorAll(".section"),
+      totalSection = allSection.length;
+      for(let i=0; i<totalNavList; i++){
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function(){
+          // for(let i=0; i<totalSection; i++){
+          //   allSection[i].classList.remove("back-section")
+          // }
+          removeBackSection();
+          for(let j=0; j<totalNavList; j++){
+            if(navList[j].querySelector("a").classList.contains("active")){
+              // console.log("back-section" + navList[i].querySelector("a"))
+              addBackSection(j);
+              // allSection[j].classList.add("back-section");
+            }
+            navList[j].querySelector("a").classList.remove("active");          }
+          this.classList.add("active")
+          showSection(this);
+          if(window.innerWidth < 1200){
+            asideSectionTogglerBtn();
+          }
+          // console.log(this)
+        })
       }
-      this.classList.add("active");
-      showSection(this);
-    })
-  }
-  function showSection(element){
-    const href = element.getAttribute("href").split("#")[1];
-    // target = href[1];
+      function removeBackSection(){
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("back-section")
+          }
+      }
+      function addBackSection(num){
+        allSection[num].classList.add("back-section")
+      }
 
-    console.log(target)
-    document.querySelector("#" + target).classList.add("active")
-  }
+      function showSection(element){
+        for(let i=0; i<totalSection; i++){
+          allSection[i].classList.remove("active")
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        
+        // target = href[1];
+        // console.log(target)
+        document.querySelector("#" + target).classList.add("active")
+      }
+      function updateNav(element){
+        // console.log(element.getAttribute("href").split("#")[1])
+        for(let i=0; i<totalNavList; i++){
+          navList[i].querySelector("a").classList.remove("active");
+          const target = element.getAttribute("href").split("#")[1];
+          if(target === navList[i].querySelector('a').getAttribute("href").split("#")[1]){
+            navList[i].querySelector("a").classList.add("active");
+          }
+        }
+      }
+      document.querySelector(".hire-me").addEventListener("click", function(){
+        const sectionindex = this.getAttribute("data-section-index");
+        // console.log(sectionindex)
+        showSection(this);
+        updateNav(this);
+        removeBackSection();
+        addBackSection(sectionindex)
+      })
+
+      const navTogglerBtn = document.querySelector(".nav-toggler"),
+            aside = document.querySelector(".aside");
+            navTogglerBtn.addEventListener("click", () =>{
+              asideSectionTogglerBtn();
+            })
+            function asideSectionTogglerBtn(){
+              aside.classList.toggle("open");
+              navTogglerBtn.classList.toggle("open");
+              for(let i=0; i<totalSection; i++){
+                allSection[i].classList.toggle("open");
+              }
+            }
